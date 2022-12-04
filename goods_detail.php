f3533a597c1b0261e2f09a09dd930706c9a64bb3
지민 백 <s2104@e-mirim.hs.kr>
2022-12-04 18:58:13 +0900

    옵션 있는 상품이면 해당 값이 들어가있는 select 추가

--- goods_detail.php
+++ goods_detail.php
@@ -2,42 +2,10 @@
     $id = $_GET['id'];
     include("db_conn.php");
 
-    // mysqli_query($conn, "insert into goods values
-    //      (1, '[러버랩] 고무장갑 팔목라인',           2000,  null,  1, 0, 0), 
-    //      (2, '[지구샵] 대나무칫솔 (10개입)',         24900, 38000, 0, 1, 0) , 
-    //      (3, '[지구샵] 주방비누',                    4900,  null,  0, 0, 1), 
-    //      (4, '[수퍼빈] 잘떼스틱',                    13000, null,  1, 0, 0), 
-    //      (5, '[지구샵] 오리지널 고체 치약 450정입',  35900, 40200, 0, 1, 1), 
-    //      (6, '[지구샵] 천연물방울수세미(3개입)',     9900,  12000, 0, 1, 1), 
-    //      (7, '[탄소창고] 손편한 앞접시 세트(5개입)', 15000, null,  1, 0, 0), 
-    //      (8, '[곡물집] 로스티드 그레인 파우더',      3000,  null,  1, 0, 1),
-    //      (9, '[빌리스벳] 프로바이오틱스 원 395',     29000, null,  1, 0, 0)        
-    //      ;");
-
-// mysqli_query($conn, "update goods set benefit = '60' where id = 1;");
-// mysqli_query($conn, "update goods set benefit = '498' where id = 2;");
-// mysqli_query($conn, "update goods set benefit = '98' where id = 3;");
-// mysqli_query($conn, "update goods set benefit = '260' where id = 4;");
-// mysqli_query($conn, "update goods set benefit = '600' where id = 5;");
-// mysqli_query($conn, "update goods set benefit = '198' where id = 6;");
-// mysqli_query($conn, "update goods set benefit = '330' where id = 7;");
-// mysqli_query($conn, "update goods set benefit = '240' where id = 8;");
-// mysqli_query($conn, "update goods set benefit = '580' where id = 9;");
-
-// mysqli_query($conn, "update goods set  = ''");
-// mysqli_query($conn, "update goods set  = ''");
-// mysqli_query($conn, "update goods set  = ''");
-// mysqli_query($conn, "update goods set  = ''");
-// mysqli_query($conn, "update goods set  = ''");
-// mysqli_query($conn, "update goods set  = ''");
-// mysqli_query($conn, "update goods set  = ''");
-// mysqli_query($conn, "update goods set  = ''");
-// mysqli_query($conn, "update goods set  = ''");
-
 //상품 상세 정보 데이터 불러오기
     $query = "select * from goods where id='$id'";
-    $arr = mysqli_query($conn, $query);
-    $goods_data_row = mysqli_fetch_row($arr);
+    $goods_arr = mysqli_query($conn, $query);
+    $goods_data_row = mysqli_fetch_row($goods_arr);
 
     $title = $goods_data_row[1];
     $ex = $goods_data_row[2];
@@ -52,15 +20,16 @@
     $new =  $goods_data_row[11];
     $sale = $goods_data_row[12];
     $best =  $goods_data_row[13];
+    $img_path = $goods_data_row[14];
 
-    // echo "<script>alert('$isoption');</script>";
-
-//상품 이미지 불러오기
-    $query = "select * from goods_img where id='$id'";
-    $arr = mysqli_query($conn, $query);
-    $img_data_row = mysqli_fetch_row($arr);
-
-    $img_path = $img_data_row[4];
+//상품 옵션 정보 있으면 데이터 불러오기
+    if($isoption){
+        $query = "select * from goods_option where g_id='$id'";
+        $options_arr = mysqli_query($conn, $query);
+        $options_data_row = mysqli_fetch_row($options_arr);
+        $options_string = $options_data_row[1]; //옵션 정보를 .으로 구분하여 하나의 문자열에 다 넣음
+        $options_value = explode('.', $options_string); //배열 방마다 option 들어있음
+    }
     
 ?>
 
@@ -83,56 +52,56 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     
     <header>
-    <!--nav-->
-    <!--로고 있는 top nav-->
-    <div class="top_nav container mt-2 mt-sm-0 mt-md-0 mt-lg-0">
-        <div class="row justify-content-between pt-lg-3 pt-sm-2 me-xxl-4 me-lg-0">
-            <div class="nav_logo col-4 col-xs-1 ps-lg-4">
-                <a href="index.php"><img class="top_nav_logo_img mt-1 pt-md-1 wd-75"
-                        src="https://cdn.imweb.me/thumbnail/20220504/5fd02ac97337a.png" alt="지구샵"
-                        width="158.535211268"></a>
-            </div>
-            <div
-                class="nav_nav col-4 col-xs-11 col-sm-6 col-md-5 pt-lg-2 pt-md-1 pt-sm-2 mt-lg-0 me-xxl-4 me-lg-1 me-2 pe-lg-0 pe-xl-4 px-0">
-                <a href="#" class="hidden login"><i class="fa-solid fa-arrow-right-to-bracket"></i></a>
-                <a href="login.html" class="sign_in_out"><span class="p-2" id="login">로그인</span></a>
-                <a href="#" class=""><span class="p-2" id="join" onclick="onclickLogout();">회원가입</span></a>
-                <a href="shop_cart.php"><span class="p-2"><i class="fa-solid fa-bag-shopping"></i></span></a>
-                <a href="#" class="hidden search"><i class="fa-solid fa-magnifying-glass"></i></a>
-            </div>
-        </div>
-    </div>
-    <!--//로고 있는 top nav-->
-
-    <!--메인 nav-->
-    <nav class="main_nav container mt-4 mb-3">
-        <div class="row">
-            <div class="left_nav col-lg-6 ms-lg-3 ms-md-0 ms-1 ms-sm-0 ms-md-0 ms-lg-0">
-                <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>MADE</span></a>
-                <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>장보기</span></a>
-                <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>지구소개</span></a>
-                <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>게시판</span></a>
-                <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>콘텐츠</span></a>
-                <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>제안하기</span></a>
-            </div>
-            <div class="right_nav col-lg-3 pt-1">
-                <a href="#" class="pe-2"><span>기획전</span></a>
-                <a href="#"><span>커뮤니티</span></a>
-            </div>
-            <div class="search_area col-lg-3">
-                <div class="search_type">
-                    <form>
-                        <input class="ps-3" type="text" placeholder="Search">
-                        <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
-                    </form>
+        <!--nav-->
+        <!--로고 있는 top nav-->
+        <div class="top_nav container mt-2 mt-sm-0 mt-md-0 mt-lg-0">
+            <div class="row justify-content-between pt-lg-3 pt-sm-2 me-xxl-4 me-lg-0">
+                <div class="nav_logo col-4 col-xs-1 ps-lg-4">
+                    <a href="index.php"><img class="top_nav_logo_img mt-1 pt-md-1 wd-75"
+                            src="https://cdn.imweb.me/thumbnail/20220504/5fd02ac97337a.png" alt="지구샵"
+                            width="158.535211268"></a>
                 </div>
-                <!--//search_type-->
+                <div
+                    class="nav_nav col-4 col-xs-11 col-sm-6 col-md-5 pt-lg-2 pt-md-1 pt-sm-2 mt-lg-0 me-xxl-4 me-lg-1 me-2 pe-lg-0 pe-xl-4 px-0">
+                    <a href="#" class="hidden login"><i class="fa-solid fa-arrow-right-to-bracket"></i></a>
+                    <a href="login.html" class="sign_in_out"><span class="p-2" id="login">로그인</span></a>
+                    <a href="#" class=""><span class="p-2" id="join" onclick="onclickLogout();">회원가입</span></a>
+                    <a href="shop_cart.php"><span class="p-2"><i class="fa-solid fa-bag-shopping"></i></span></a>
+                    <a href="#" class="hidden search"><i class="fa-solid fa-magnifying-glass"></i></a>
+                </div>
             </div>
-            <!--//search_area-->
         </div>
-    </nav>
-    <!--//메인 nav-->
-    <!--//nav-->
+        <!--//로고 있는 top nav-->
+
+        <!--메인 nav-->
+        <nav class="main_nav container mt-4 mb-3">
+            <div class="row">
+                <div class="left_nav col-lg-6 ms-lg-3 ms-md-0 ms-1 ms-sm-0 ms-md-0 ms-lg-0">
+                    <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>MADE</span></a>
+                    <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>장보기</span></a>
+                    <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>지구소개</span></a>
+                    <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>게시판</span></a>
+                    <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>콘텐츠</span></a>
+                    <a class="pe-lg-3 pe-md-1 pe-sm-2" href="#"><span>제안하기</span></a>
+                </div>
+                <div class="right_nav col-lg-3 pt-1">
+                    <a href="#" class="pe-2"><span>기획전</span></a>
+                    <a href="#"><span>커뮤니티</span></a>
+                </div>
+                <div class="search_area col-lg-3">
+                    <div class="search_type">
+                        <form>
+                            <input class="ps-3" type="text" placeholder="Search">
+                            <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+                        </form>
+                    </div>
+                    <!--//search_type-->
+                </div>
+                <!--//search_area-->
+            </div>
+        </nav>
+        <!--//메인 nav-->
+        <!--//nav-->
     </header>
 
 
@@ -178,15 +147,23 @@
                     </div>
                 </div><!--//goods_detailed_info-->
                 <div class="goods_buy mt-3 pe-3">
+                    <?php
+                        if($isoption){
+                    ?>
                     <div class="option" id="option">
                         <span class="fw-bold" style="font-size: 0.7em" id="option">옵션 *</span><br>
                         <select class="form-select goods_select mt-2" id="option">
-                            <option selected>선택해주세요.</option>
-                            <option value="ultimate">얼티밋</option>
-                            <option value="coral">코랄</option>
-                            <option value="almostwinter">얼모스트블루</option>
+                            <option selected disabled>선택해주세요.</option>
+                            <?php
+                                for($i = 0; $i<sizeof($options_value); $i++)
+                                    echo "<option value='$options_value[$i]'>$options_value[$i]</option>";
+                            ?>
                         </select>
                     </div><!--//option-->
+                    <?php
+                        }
+                    ?>
+
                     <div class="select_block my-3 p-3 bg-secondary bg-opacity-10 rounded">
                         <div class="option_title row justify-content-between">
                             <span class="col text-muted" style="font-size: 0.9em">옵션 이름</span>
@@ -354,10 +331,4 @@ if(strcmp($best, "1"))
     let bestIcon  = document.getElementById('BESTicon');
     bestIcon.setAttribute('style', 'display:none;');
     </script>";    
-
-if(!$isoption)
-echo "<script>
-let option  = document.getElementById('option');
-option.setAttribute('style', 'display:none;');
-</script>"; 
 ?>
